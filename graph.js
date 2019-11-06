e7b7d413204407e188ca1516effa5e583d022e5a
Jerry <j.ross@centre.edu>
2019-11-06 13:00:37 -0500

    ok

--- graph.js
+++ graph.js
@@ -1,1 +1,41 @@
 
+pengdatapromise = d3.json("classData.json")
+
+pengdatapromise.then(function (data)
+{
+    console.log ("good", (data))
+    
+},
+function (err)
+{
+    console.log("error", err)
+})
+
+
+var makebuttons = function(day)
+{}
+
+
+
+
+
+var xs = d3.range(10)
+
+var points = xs.map(function (x)
+{
+    return {
+        x:x.n,
+        y:y
+    }
+    
+})
+
+//var screensize = {width:300px, height:600px}
+
+var getquizgrade = function (quiz)
+{
+    
+}
+                  
+                  
+        
