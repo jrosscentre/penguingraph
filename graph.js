832fb7feaeb608b6a1d96d241ee9138006d0a621
Jerry <j.ross@centre.edu>
2019-11-06 18:26:42 -0500

    next

--- graph.js
+++ graph.js
@@ -1,41 +1,87 @@
+pengdatapromise = d3.json("penguins/penguins/classData.json");
 
-pengdatapromise = d3.json("classData.json")
+pengdatapromise.then(
 
-pengdatapromise.then(function (data)
-{
-    console.log ("good", (data))
-    
-},
+function(data)
+    {
+        console.log("good", (data)),
+        console.log(getquizgrades(data)),
+        console.log(generatepoints(data))
+
+    },
 function (err)
-{
-    console.log("error", err)
-})
-
-
-var makebuttons = function(day)
-{}
-
-
-
-
-
-var xs = d3.range(10)
-
-var points = xs.map(function (x)
-{
-    return {
-        x:x.n,
-        y:y
+    {
+        console.log("broken", (err))
     }
+    )
+
+
+
+var get1quiz = function (peng)
+{
+    console.log(peng)
+    return peng.quizes[0].grade;
     
-})
+}
+
+
+var getquizgrades = function (data)
+{
+    console.log("good")
+    return data.map(get1quiz)
+}
+
+
+//var makebuttons = function(day)
+//{}
+
+
+
+var generatepoints = function (quizgds)
+{
+    
+    var arraycoord = quizgds.map(function(quizgd,index)
+    {
+        
+        var point= {}
+        point.y = quizgd.quizes[0].grade
+        point.x = index
+        return point
+        
+    })
+    
+    return arraycoord
+}
+
+var setup = function (points)
+{
+    d3.select ("svg")
+    .attr()
+    
+    var xscale = d3.scalelinear()
+    xscale.domain ([
+        d3.min (points, function (p) {return p.x}),
+        d3.max (points, function (p) {return p.x})
+    xscale.range ([])
+    
+    var yscale = d3.scalelinear()
+    yscale.domain([
+        d3.min (points, function (p) {return p.y}),
+        d3.max (points, function (p) {return p.y})
+    ]
+        d3.min(generatepoints(points.y))
+
+}
+
+
+
+
+
+
 
 //var screensize = {width:300px, height:600px}
 
-var getquizgrade = function (quiz)
-{
-    
-}
+
                   
                   
         
